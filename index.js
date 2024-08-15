7fdef29a1c8938daaa93347c01410638f7729355
hrmrakib <hrmrakibs@gmail.com>
2024-08-15 09:35:03 +0600

    initial set-up server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is on ${port}`);
+});
